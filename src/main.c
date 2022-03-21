50816a5cf413e7ca191da389fdcf2355c468376d
flufster4 <markian.vrublevsky@gmail.com>
2022-03-20 20:41:02 -0500

    Added source code.

---
+++ src/main.c
@@ -0,0 +1,296 @@
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define false 0
+#define true 1
+#define null 0
+
+//gtk_label_new("");
+//gtk_button_new_with_label("");
+//gtk_entry_set_placeholder_text(GTK_ENTRY(entry),"placeholder");
+//gtk_entry_new();
+//gtk_widget_set_size_request(wiget, x, y);
+//gtk_entry_set_visibility(GTK_ENTRY(entry), bool);
+//gtk_entry_get_text(GTK_ENTRY(entry)); 
+//gtk_label_set_text(GTK_LABEL(label),gchar); 
+// button event registering function - void button_clicked(GtkWidget *widget,gpointer data)
+// contect button to register - g_signal_connect(button,"clicked",G_CALLBACK(function),NULL);
+
+GtkWidget *k, *path, *name, *texttype, *imagetype, *soundtype;
+
+int remove_char(char *s, int pos)
+{
+  int length = strlen(s);
+  
+  if (pos >= length) return false;
+  
+  for (int i = pos; i < length; i++)
+    s[i] = s[i + 1];
+  
+  return true;
+}
+
+static void destroy(GtkWidget *widget, gpointer data)
+{
+	gtk_main_quit();
+}
+
+void additem(GtkWidget *item, int x, int y) {
+	gtk_fixed_put (GTK_FIXED (k), item,x,y);
+}
+
+void selectfile() {
+	GtkWidget       *fc_dialog;
+	GtkFileChooser  *fc;
+	char            *filename;
+
+	fc_dialog = gtk_file_chooser_dialog_new("Choose File", NULL, 		GTK_FILE_CHOOSER_ACTION_OPEN, 	"Cancel", GTK_RESPONSE_CANCEL, "Open", GTK_RESPONSE_ACCEPT, NULL);
+
+	gtk_widget_show(fc_dialog);
+
+	if (gtk_dialog_run(GTK_DIALOG(fc_dialog)) == GTK_RESPONSE_ACCEPT){
+     		fc = GTK_FILE_CHOOSER(fc_dialog);
+     		filename = gtk_file_chooser_get_filename(fc);
+     		gtk_entry_set_text(GTK_ENTRY(path),filename);
+     		g_free (filename);
+     		gtk_widget_destroy(fc_dialog);}
+	else {
+     		gtk_widget_destroy(fc_dialog);
+     	}
+}
+
+void filebutton_clicked(GtkWidget *widget,gpointer data) {
+	selectfile();
+}
+
+void makebutton_clicked(GtkWidget *widget,gpointer data) {
+	//todo
+	if (fopen(gtk_entry_get_text(GTK_ENTRY(path)),"r") == NULL) {
+		g_print("Invalid path!\n");
+	}
+	else {
+		//check if path is not null
+		if (strlen(gtk_entry_get_text(GTK_ENTRY(name))) == 0) return;
+		int namelen = strlen(gtk_entry_get_text(GTK_ENTRY(name)));
+
+		//checks for valid path
+		char namepath[4096] = "";
+		strcpy(namepath, gtk_entry_get_text(GTK_ENTRY(name)));
+
+		for (int i = namelen; gtk_entry_get_text(GTK_ENTRY(name))[i] != '/'; i--) {
+			remove_char(namepath, i);
+		}
+		int pathlen = strlen(namepath);
+		if (opendir(namepath) == null) return;
+
+		printf("valid path!\n");
+
+		//save file
+		int type = 0;
+		//type 0 - image
+		//type 1 - audio/video
+		//type 2 - other
+
+		if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(imagetype)) == 1) type = 0;
+		if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(soundtype)) == 1) type = 1;
+		if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(texttype)) == 1) type = 2;
+
+		FILE *shortcut = fopen(gtk_entry_get_text(GTK_ENTRY(name)),"w");
+		fprintf(shortcut, "[shortcut]\npath=%s\ntype=%d",gtk_entry_get_text(GTK_ENTRY(path)),type);
+		fclose(shortcut);
+
+		//quit app
+		gtk_main_quit();
+
+		GtkWidget *dialog;
+		GtkDialogFlags flags = GTK_DIALOG_DESTROY_WITH_PARENT;
+ 		dialog = gtk_message_dialog_new (NULL,
+                                  flags,
+                                  GTK_MESSAGE_INFO,
+                                  GTK_BUTTONS_OK,
+								  "Succsesfully made shortcut!");
+ 		gtk_dialog_run (GTK_DIALOG (dialog));
+ 		gtk_widget_destroy (dialog);
+	}
+ }
+
+void selectname_clicked(GtkWidget *widget,gpointer data) {
+	GtkWidget       *fc_dialog;
+	GtkFileChooser  *fc;
+	char            *filename;
+
+	fc_dialog = gtk_file_chooser_dialog_new("Shortcut Name", NULL, GTK_FILE_CHOOSER_ACTION_SAVE, 	"Cancel", GTK_RESPONSE_CANCEL, "Save", GTK_RESPONSE_ACCEPT, NULL);
+
+	gtk_widget_show(fc_dialog);
+
+	if (gtk_dialog_run(GTK_DIALOG(fc_dialog)) == GTK_RESPONSE_ACCEPT){
+     		fc = GTK_FILE_CHOOSER(fc_dialog);
+     		filename = gtk_file_chooser_get_filename(fc);
+			char suffix[4] = ".lnk"; 
+
+			strcat(filename, suffix);
+     		gtk_entry_set_text(GTK_ENTRY(name),filename);
+
+     		g_free (filename);
+     		gtk_widget_destroy(fc_dialog);}
+	else {
+     		gtk_widget_destroy(fc_dialog);
+    }
+}
+
+void quitbutton_clicked(GtkWidget *widget,gpointer data) {
+	gtk_main_quit();
+}
+
+/*
+char * ini_getvalue(FILE * file, int rnewline, char * key, char * value) {
+	char line[5100] = "";
+
+	//loops tru file
+	while (fgets(line, 5100, file) != NULL) {
+		char substr[5100] = "";
+
+		if (strcmp(strncpy(substr, &line[0], strlen(key) + 1),key) == 0) {
+			strncpy(value, &line[strlen(substr)], strlen(line));
+					
+					if (rnewline == 1) value[strcspn(value, "\n")] = 0;
+					return value;
+
+				}
+			}
+}
+*/
+
+int main(int argc, char *argv[])
+{
+	
+	if (argc == 2) {
+		if (fopen(argv[1], "r")) {
+			FILE *file = fopen(argv[1], "r");
+			char line[5100] = "";
+			char value[5100] = "";
+			char path[5100] = "";
+			char type[2] = "";
+
+			while (fgets(line, 5100, file) != NULL) {
+				char substr[5100] = "";
+				if (strcmp(strncpy(substr, &line[0], 5),"path=") == 0) {
+					strncpy(path, &line[strlen(substr)], strlen(line));
+					
+					path[strcspn(path, "\n")] = 0;
+
+				}
+
+				if (strcmp(strncpy(substr, &line[0], 5),"type=") == 0) {
+					strncpy(type, &line[strlen(substr)], strlen(line));
+					
+					type[strcspn(type, "\n")] = 0;
+
+				}
+				
+			}
+			printf("path: %s\ntype: %s", path, type);
+
+			if (strcmp(type, "2") == 0) {
+					char cmd[9] = "gedit \"";
+					strcat(cmd, path);
+					strcat(cmd, "\"");
+					printf("path: %s", path);
+					printf("cmd: %s", cmd);
+					system(cmd);
+			}
+
+
+			if (strcmp(type, "1") == 0) {
+					char cmd[13] = "xdg-open \"";
+					strcat(cmd, path);
+					strcat(cmd, "\"");
+					printf("path: %s", path);
+					printf("cmd: %s", cmd);
+					system(cmd);
+			}
+
+
+			if (strcmp(type, "0") == 0) {
+					char cmd[9] = "eog \"";
+					strcat(cmd, path);
+					strcat(cmd, "\"");
+					printf("path: %s", path);
+					printf("cmd: %s", cmd);
+					system(cmd);
+			}
+
+			fclose(file);
+			return 0;
+		}
+		return EXIT_FAILURE;
+	}
+	
+	//make window
+	gtk_init(&argc, &argv);
+	GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_window_set_title(GTK_WINDOW(window), "Linux Cuts Dev");
+	g_signal_connect(window, "destroy", G_CALLBACK(destroy), NULL);
+
+	k= gtk_fixed_new();
+    	gtk_container_add(GTK_CONTAINER(window), k);
+    	
+    	//name field
+    GtkWidget *namelabel, *pathlabel, *openfile, *make, *cancel, *savename, *typelabel;
+
+	namelabel = gtk_label_new("Name:");
+	name = gtk_entry_new();
+	gtk_entry_set_placeholder_text(GTK_ENTRY(name),"shortcut name");
+	
+	//path field
+	pathlabel = gtk_label_new("Path:");
+	path = gtk_entry_new();
+	gtk_entry_set_placeholder_text(GTK_ENTRY(path),"target path");
+	
+	//open file button
+	openfile = gtk_button_new_with_label("📁");
+	g_signal_connect(openfile,"clicked",G_CALLBACK(filebutton_clicked),NULL);
+
+	savename = gtk_button_new_with_label("📂");
+	g_signal_connect(savename,"clicked",G_CALLBACK(selectname_clicked),NULL);
+	
+	cancel = gtk_button_new_with_label("Cancel");
+	g_signal_connect(cancel,"clicked",G_CALLBACK(quitbutton_clicked),NULL);
+	
+	make = gtk_button_new_with_label("Create Shortcut");
+	g_signal_connect(make,"clicked",G_CALLBACK(makebutton_clicked),NULL);
+
+	//file type
+	typelabel = gtk_label_new("File type:");
+
+	imagetype = gtk_radio_button_new_with_label(NULL,"Image");
+	soundtype = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(imagetype), "Video/Audio");
+	texttype = gtk_radio_button_new_with_label_from_widget(GTK_RADIO_BUTTON(imagetype), "Other");
+    	
+    	//add items to GUI
+    	additem(namelabel, 25, 33);
+    	additem(name, 85,25);
+    	additem(pathlabel, 25, 80);
+    	additem(path, 85, 75);
+    	additem(openfile, 275, 75);
+    	additem(cancel, 15, 190);
+    	additem(make, 200, 190);
+		additem(savename, 275, 25);
+		additem(typelabel,25,125);
+		additem(imagetype, 25,155);
+		additem(soundtype, 100, 155);
+		additem(texttype, 215, 155);
+  
+	//set window size & show window
+    	gtk_widget_set_size_request(GTK_WIDGET(window),350,240);
+	gtk_widget_show_all(GTK_WIDGET(window));
+	
+	
+	
+
+gtk_main();
+	return 0;
+}
+ 
